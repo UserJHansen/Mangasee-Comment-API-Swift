152bf9b2208633890b4ae972af2bacc0082b33e3
Charlie Welsh <charlie@allotrope.dev>
2023-01-21 01:48:30 -0800

    Co-authored-by: JHansen <UserJHansen@users.noreply.github.com>

--- Sources/App/Models/Manga.swift
+++ Sources/App/Models/Manga.swift
@@ -1,7 +1,17 @@
 import Fluent
 import Vapor
 
-final class Manga: Model, Content {
+final class Manga: Model, Content, Hashable, Equatable {
+    static func == (lhs: Manga, rhs: Manga) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+
+
+
   static let schema = "mangas"
 
   @ID(custom: .id, generatedBy: .user)
--- Sources/App/Scanner/getMangaComments.swift
+++ Sources/App/Scanner/getMangaComments.swift
@@ -1,6 +1,8 @@
 import AsyncHTTPClient
 import Fluent
 import Vapor
+import Foundation
+import CollectionConcurrencyKit
 
 struct SearchResponse: Codable {
     let i: String // Indexable name
@@ -9,91 +11,103 @@ struct SearchResponse: Codable {
 }
 
 extension ScanHandler {
-    func getMangaComments() async {
-        var mangaNames = Set<String>()
+    /// Returns a set of every available manga's name.
+    func pullNamesFromServer() async throws -> Set<String> {
         var data: ByteBuffer = .init()
 
-        do {
-            let response: HTTPClientResponse = try await client.execute(.init(url: "\(server)_search.php"), timeout: .seconds(30))
-            data = try await response.body.collect(upTo: 1024 * 1024)
-            let mangas = try JSONDecoder().decode([SearchResponse].self, from: data)
+        let response: HTTPClientResponse = try await client.execute(.init(url: "\(server)_search.php"), timeout: .seconds(30))
+        data = try await response.body.collect(upTo: 1024 * 1024)
+        let mangas = try JSONDecoder().decode([SearchResponse].self, from: data)
 
-            logger.info("\(Date()) Downloaded list of \(mangas.count) mangas")
+        logger.info("\(Date()) Downloaded list of \(mangas.count) mangas")
 
-            mangaNames = Set(mangas.map { $0.i })
-        } catch {
-            logger.error("\(Date()) Error getting mangas: \(error) Data \(data.getString(at: 0, length: 1024)!)")
-            return
-        }
+        return Set(mangas.map { $0.i })
+    }
 
-        let queue = mangaNames.map {
+    /// Takes a Set of manga names, then generates the associated HTTPClientRequest, and returns both.
+    func generateRequestQueue(mangaNames: Set<String>) async throws -> [(name: String, request: HTTPClientRequest)] {
+        return mangaNames.map { name in
             var request = HTTPClientRequest(url: "\(server)manga/comment.get.php")
-            request.body = .bytes(.init(string: "{\"IndexName\": \"\($0)\"}"))
+            // Not using an encoder here for performance reasons
+            request.body = .bytes(.init(string: """
+            {"IndexName": "\(name)"}
+            """
+            ))
             request.method = .POST
 
-            return ($0, request)
+            return (name, request)
         }
+    }
 
+    /// Takes a manga HTTPClientRequest and returns the comments from said request.
+    func requestMangaComments(request req: HTTPClientRequest) async throws -> [RawComment] {
+        let response = try await client.execute(req, timeout: .seconds(60))
+        let data = try await response.body.collect(upTo: 1024 * 1024)
+
+        return try JSONDecoder().decode(MangaseeResponse<[RawComment]>.self, from: data).val
+    }
+
+    /// Gets more replies to the given comment.
+    func requestMoreReplies(to comment: RawComment) async throws -> [RawReply] {
+        var request = HTTPClientRequest(url: "\(server)manga/reply.get.php")
+        // Not using an encoder here for performance reasons
+        request.body = .bytes(.init(string: """
+        {"TargetID": "\(comment.CommentID)}"
+        """))
+        request.method = .POST
+        let response = try await client.execute(request, timeout: .seconds(30))
+        let data = try await response.body.collect(upTo: 1024 * 1024)
+        let decodedBody =  try JSONDecoder().decode(MangaseeResponse<[RawReply]>.self, from: data)
+
+        return decodedBody.val
+    }
+
+    func getMangaComments() async throws {
+        let mangaNames = try await pullNamesFromServer()
+
+        let queue = try await generateRequestQueue(mangaNames: mangaNames)
+
+        // Add all the users from the database to the user buffer
         let usernames = UserBuffer()
         for user in try! await (User.query(on: db).all()) {
             await usernames.addUser(user)
         }
 
         logger.info("\(Date()) Building \(queue.count) requests...")
+        // Set up our in-memory storage for comments and replies
         let comments = CommentBuffer<String>()
         let replies = ReplyBuffer()
 
-        await queue.limitedConcurrentForEach(maxConcurrent: ConLimits.network) { [self] id, url in
-            logger.debug("Starting request for manga \(id)")
-
-            let data: ByteBuffer
-            do {
-                let response = try await client.execute(url, timeout: .seconds(60))
-                data = try await response.body.collect(upTo: 1024 * 1024)
-            } catch {
-                logger.error("\(Date()) Error getting comments for manga \(id): \(error)")
-                return
-            }
-
-            let commentList: [RawComment]
-            do {
-                commentList = try JSONDecoder().decode(MangaseeResponse<[RawComment]>.self, from: data).val
-            } catch {
-                logger.error("\(Date()) Error decoding comments for manga \(id): \(error) \(data.getString(at: 0, length: data.readableBytes)!)")
-                return
-            }
-
+        // NOTE: This may potentially cancel on a failed request. Make sure to test.
+        try await queue.limitedConcurrentForEach(maxConcurrent: ConLimits.network) { [self] id, req in
+            let commentList = try await requestMangaComments(request: req)
+            
             for comment in commentList {
                 logger.debug("Adding \(comment.Username) (\(comment.UserID)) to usernames")
                 await usernames.addUser(User(id: Int(comment.UserID)!, name: comment.Username))
                 await comments.addComment(id, comment)
 
-                if Int(comment.ReplyCount)! != comment.Replies.count {
+                // TODO: Move all this to Comment.
+                guard let replyCount = Int(comment.ReplyCount) else {
+                    // TODO: Add actual errors here.
+                    return
+                }
+
+                let replies: [RawReply]
+
+                if replyCount != comment.Replies.count {
                     logger.debug("\(Date()) Loading replies for comment \(comment.CommentID) in manga \(id) ( \(comment.Replies.count) / \(comment.ReplyCount) replies)")
-
-                    do {
-                        var request = HTTPClientRequest(url: "\(server)manga/reply.get.php")
-                        request.body = .bytes(.init(string: "{\"TargetID\": \(comment.CommentID)}"))
-                        request.method = .POST
-                        let response = try await client.execute(request, timeout: .seconds(30))
-                        let data = try await response.body.collect(upTo: 1024 * 1024)
-                        let newReplies = try JSONDecoder().decode(MangaseeResponse<[RawReply]>.self, from: data).val
-
-                        for reply in newReplies {
-                            logger.debug("Adding \(reply.Username) (\(reply.UserID)) to usernames")
-                            await usernames.addUser(User(id: Int(reply.UserID)!, name: reply.Username))
-                            await replies.addReply(Int(comment.CommentID)!, reply)
-                            logger.debug("Added reply \(reply.CommentID) to comment \(comment.CommentID)")
-                        }
-                    } catch {
-                        logger.error("\(Date()) Error loading replies: \(error)")
-                    }
+                    replies = try await requestMoreReplies(to: comment)
                 } else {
-                    for reply in comment.Replies {
-                        logger.debug("Adding \(reply.Username) (\(reply.UserID)) to usernames")
-                        await usernames.addUser(User(id: Int(reply.UserID)!, name: reply.Username))
-                        await replies.addReply(Int(comment.CommentID)!, reply)
-                    }
+                    logger.debug("\(Date()) Comment \(comment.CommentID) already had all replies in manga \(id) ( \(comment.Replies.count) / \(comment.ReplyCount) replies)")
+                    replies = comment.replies
+                }
+
+                for reply in replies {
+                    logger.debug("Adding \(reply.Username) (\(reply.UserID)) to usernames")
+                    await usernames.addUser(User(id: Int(reply.UserID)!, name: reply.Username))
+                    await replies.addReply(Int(comment.CommentID)!, reply)
+                    logger.debug("Added reply \(reply.CommentID) to comment \(comment.CommentID)")
                 }
             }
         }
@@ -108,18 +122,19 @@ extension ScanHandler {
             try? await user.save(on: db)
         }
 
-        let preexistingmangas = try! await Manga.query(on: db).all()
-        await mangaNames.limitedConcurrentForEach(maxConcurrent: ConLimits.db) { [self] manga async in
-            if preexistingmangas.contains(where: { $0.id == manga }) {
-                logger.debug("Skipping \(manga)")
+        let preexistingManga = Set(try await Manga.query(on: db).all().concurrentMap { manga in
+            try manga.requireID()
+        })
+
+        try await mangaNames.limitedConcurrentForEach(maxConcurrent: ConLimits.db) { [self] mangaName async in
+
+            guard !preexistingManga.contains(mangaName) else {
+                logger.debug("Skipping \(mangaName)")
                 return
             }
-            logger.debug("Saving \(manga)")
-            do {
-                try await Manga(id: manga).save(on: db)
-            } catch {
-                logger.error("Failed to save manga \(manga): \(error)")
-            }
+
+            logger.debug("Saving \(mangaName)")
+            try! await Manga(id: mangaName).save(on: db)
         }
 
         logger.info("\(Date()) Saving comments")
